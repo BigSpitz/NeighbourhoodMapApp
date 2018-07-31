5d006ce3d8e1a657954869a3a4be926365a0cd47
Nick Xeras <nikos_npx@yahoo.gr>
2018-07-31 23:55:37 +0300

    map markers are in place

--- src/App.js
+++ src/App.js
@@ -3,43 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import GoogleMaps from './googleMaps';
 
-
-
-
-
 class App extends Component {
-
-
   state = {
     coffeeArray: []
   }
-
-
-
 
  foursquareGetCoffee() {
    fetch(`https://api.foursquare.com/v2/venues/search?ll=37.9784901,23.7118831&radius=800&query=coffee&client_id=WKYFMVAN25S40USFER3ZLDFAOWZ3ZGKLZQ0QEJU1TBOXTGKT&client_secret=OJMU5I2IU1LZWVG0IOSHRZECKMTQVXTD2WRL51KWTNNYCDVE&v=20180731`)
    .then(data => data=data.json()).then((result)=>result.response.venues.map(coffee=>this.setState(prevState => ({ coffeeArray: [...prevState.coffeeArray, [coffee.location.lat,coffee.location.lng]]}))));
  }
 
-
-
   componentDidMount() {
   this.foursquareGetCoffee();
 
   }
 
-
-
-
-
   render() {
-
     return ( <div className = "App" >
       <div className = "navbar" >
       dsfgfdgfdgd </div>
 
-      <GoogleMaps / >
+      <GoogleMaps coffeeArray={this.state.coffeeArray}/ >
       <footer > sadffffffffffffffffffffffffffffffffff
       </footer>
 
--- src/googleMaps.js
+++ src/googleMaps.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 
 
@@ -9,16 +9,20 @@ class GoogleMaps extends Component {
 
 
   render() {
-   const GoogleMapPanormou = withGoogleMap(props => (
+   const GoogleMapGazi = withGoogleMap(props => (
       <GoogleMap
         defaultCenter = { { lat: 37.9784901, lng: 23.7118831 } }
-        defaultZoom = { 16 }
+        defaultZoom = { 15 }
+
       >
+{this.props.coffeeArray.map((ll,i)=><Marker key={i} position={{ lat: ll[0], lng: ll[1] }} />)}
+
+
       </GoogleMap>
    ));
    return(
       <div>
-        <GoogleMapPanormou
+        <GoogleMapGazi
           containerElement={ <div style={{ height: `500px`, width: '100%' }} /> }
           mapElement={ <div style={{ height: `100%` }} /> }
         />
